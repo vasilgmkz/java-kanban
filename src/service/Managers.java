f29b84e490de775b47e28e7839792c9adacf0236
Vasili <vasilgmkz@yandex.kz>
2024-05-08 19:10:01 +0300

    Реализовать LinkedHashMap в классе InMemoryHistoryManager. Исправить ошибку автопроверки 2

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,7 +1,7 @@
 package service;
 
 public class Managers {
-    public static TaskManager getDefault () {
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
 
