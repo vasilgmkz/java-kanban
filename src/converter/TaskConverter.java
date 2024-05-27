3aa1a726848d3acab795f16686997f9634350b8d
Vasili <vasilgmkz@yandex.kz>
2024-05-27 11:55:16 +0300

    Исправить замечания ver.2

--- src/converter/TaskConverter.java
+++ src/converter/TaskConverter.java
@@ -16,18 +16,19 @@ public class TaskConverter {
         String[] task = value.split(",");
         final int id = Integer.parseInt(task[0]);
         final TaskType type = TaskType.valueOf(task[1]);
+        final String name = task[2];
+        final Status status = Status.valueOf(task[3]);
+        final String description = task[4];
+        final int epicId = Integer.parseInt(task[5]);
         if (type.equals(TaskType.TASK)) {
-            return new Task(id, task[2], Status.valueOf(task[3]), task[4]);
+            return new Task(id, name, status, description);
         } else if (type.equals(TaskType.EPIC)) {
-            return new Epic(id, task[2], Status.valueOf(task[3]), task[4]);
+            return new Epic(id, name, status, description);
         } else if (type.equals(TaskType.SUBTASK)) {
-            return new SubTask(id, task[2], Status.valueOf(task[3]),
-                    task[4], Integer.parseInt(task[5]));
+            return new SubTask(id, name, status, description, epicId);
         } else {
             return null;
         }
     }
-
-
 }
 
